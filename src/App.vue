6cd28a93c381588c07a4d6c9e19acfa5d47f6e84
kyoruni <kyoruni@gmail.com>
2020-06-05 17:55:24 +0900

    :heavy_minus_sign: 不要なファイル削除

--- src/App.vue
+++ src/App.vue
@@ -36,25 +36,12 @@
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-};
+}
 </script>
