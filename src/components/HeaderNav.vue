5cd993edc7b6694467b7ae2b3fcafd7eba16e9ca
kyoruni <kyoruni@gmail.com>
2020-06-06 13:30:44 +0900

    :wrench: タイトルを記載

--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="header">
     <v-toolbar :short="true" :dark="true" :color="mainColor">
+      <v-toolbar-title>あつ森アイテムチェッカー</v-toolbar-title>
     </v-toolbar>
   </div>
 </template>
