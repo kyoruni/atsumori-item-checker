1401624d67ef1734f63b4141707651340c3f6655
kyoruni <kyoruni@gmail.com>
2020-06-06 17:02:29 +0900

    :wrench: リスト表示(チェックボックス付き)

--- src/components/ItemsList.vue
+++ src/components/ItemsList.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="items-list">
     {{ category }}
+    <v-list>
+      <v-list-item-group>
+        <v-list-item v-for="item in items" :key="item.id">
+          <template v-slot:default="{ active, toggle }">
+            <v-list-item-action>
+              <v-checkbox color="primary" @click="toggle"/>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>{{ item.name }}</v-list-item-title>
+            </v-list-item-content>
+          </template>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
     <ul>
       <li v-for="item in items" :key="item.id">
         {{ item.name }}
