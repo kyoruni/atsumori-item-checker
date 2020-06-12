6df75b04461302ef3764ae222a2af1b258777ef9
kyoruni <kyoruni@gmail.com>
2020-06-12 20:57:23 +0900

    :wrench: 更新履歴を子コンポーネントに切り出し

--- src/components/About.vue
+++ src/components/About.vue
@@ -18,24 +18,7 @@
           </ul>
         </v-card-text>
         <!-- 更新履歴 -->
-        <v-list>
-          <v-list-item-group v-for="history in histories" :key="history.id" multiple>
-            <v-divider/>
-            <v-list-group :ripple="false">
-              <!-- 親のリスト -->
-              <template v-slot:activator>
-                <v-icon class="mr-2" :small="true">fas fa-pen</v-icon>
-                <v-list-item-title> version: {{ history.version }} 【{{ history.date }}】</v-list-item-title>
-              </template>
-            <!-- 子のリスト -->
-            <ul>
-              <v-list-item no-action v-for="(content, index) in history.contents" :key="index" :ripple="false">
-                <li>{{ content }}</li>
-              </v-list-item>
-            </ul>
-            </v-list-group>
-          </v-list-item-group>
-        </v-list>
+        <history/>
         <v-card-actions>
           <v-spacer></v-spacer>
           <v-btn color="primary" text @click="close()">
@@ -48,13 +31,15 @@
 </template>
 
 <script>
-import histories from '@/assets/histories.json'
+import History from '@/components/History'
 
 export default {
+  components: {
+    History,
+  },
   data() {
     return {
       dialog: false,
-      histories,
     }
   },
   methods: {
@@ -67,9 +52,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-.hoge {
-  background: #B8EEF7;
-}
-</style>
---
+++ src/components/History.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="history">
+    <v-list>
+      <v-list-item-group v-for="history in histories" :key="history.id" multiple>
+        <v-divider/>
+        <v-list-group :ripple="false">
+          <!-- 親のリスト -->
+          <template v-slot:activator>
+            <v-icon class="mr-2" :small="true">fas fa-pen</v-icon>
+            <v-list-item-title> version: {{ history.version }} 【{{ history.date }}】</v-list-item-title>
+          </template>
+        <!-- 子のリスト -->
+        <ul>
+          <v-list-item no-action v-for="(content, index) in history.contents" :key="index" :ripple="false">
+            <li>{{ content }}</li>
+          </v-list-item>
+        </ul>
+        </v-list-group>
+      </v-list-item-group>
+    </v-list>
+  </div>
+</template>
+
+<script>
+import histories from '@/assets/histories.json'
+
+export default {
+  data () {
+    return {
+      histories,
+    }
+  }
+}
+</script>
