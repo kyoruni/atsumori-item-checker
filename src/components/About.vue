db1798aabbac6545bf16bd6bf44cc9f8d4648ffb
kyoruni <kyoruni@gmail.com>
2020-06-12 21:48:40 +0900

    :wrench: About追記

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,35 +1,37 @@
 <template>
   <div class="about">
     <v-dialog v-model="dialog" scrollable width="800">
-      <v-card>
+      <v-card style="height: 400px;">
         <v-card-title primary-title>
           あつ森アイテムチェッカー（作成中）
         </v-card-title>
         <v-card-text>
-          <ul>
-            <li>一覧にないアイテムや誤字などありましたら、DMでこっそりお知らせいただけるとうれしいです！</li>
+          <h3>つくった人</h3>
+          <ul class="mb-2">
+            <li>きょるに @kyoruni</li>
           </ul>
-        </v-card-text>
-        <v-card-text>
-          <ul>
+          <ul class="mb-7">
+            <li>一覧にないアイテムや誤字などありましたら、DMやリプライでこっそりお知らせいただけるとうれしいです！</li>
+          </ul>
+          <h3>データの保存について</h3>
+          <ul class="mb-7">
             <li>チェック状況は、お使いのブラウザのローカルストレージ上に保存しています。</li>
             <li>異なる端末、ブラウザでのデータ共有はできません。</li>
             <li>ブラウザの閲覧履歴を削除すると、データが消えてしまいます。</li>
             <li>作成中のため、突然データリセットを行う可能性があります。すみません……。</li>
           </ul>
-        </v-card-text>
-        <v-card-text>
+          <h3>データを消したい</h3>
           <ul>
             <li>以下ボタンよりチェック状況を全削除することができます。</li>
           </ul>
-          <v-card-actions>
+          <v-card-actions class="mb-7">
             <v-btn depressed color="error" class="font-weight-bold" @click="deleteButton()">
               チェック状況全削除
             </v-btn>
           </v-card-actions>
+          <!-- 更新履歴 -->
+          <history/>
         </v-card-text>
-        <!-- 更新履歴 -->
-        <history/>
       </v-card>
     </v-dialog>
   </div>
